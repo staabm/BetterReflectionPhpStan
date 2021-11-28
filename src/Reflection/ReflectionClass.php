97f0422aa6be047f7fbead97e5163880710eacc7
Ondrej Mirtes <ondrej@mirtes.cz>
2021-11-28 13:41:56 +0100

    ReflectionClass::getParentClass() - be more resilient against errors

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -1166,7 +1166,11 @@ class ReflectionClass implements Reflection
 
             $parentClassName = $this->parentClassName;
             while ($parentClassName !== null) {
-                $parentClass = $this->reflector->reflectClass($parentClassName);
+                try {
+                    $parentClass = $this->reflector->reflectClass($parentClassName);
+                } catch (IdentifierNotFound) {
+                    break;
+                }
 
                 if (
                     $this->name === $parentClassName
