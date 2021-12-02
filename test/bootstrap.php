6adf1bf13c0acab62af35167cc0109382fd61621
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-02 13:35:00 +0100

    Load enum stubs only on PHP 8.0+

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,4 +2,15 @@
 
 declare(strict_types=1);
 
+if (PHP_VERSION_ID >= 80000) {
+    require __DIR__ . '/../stubs/UnitEnum.php';
+    require __DIR__ . '/../stubs/BackedEnum.php';
+
+    require __DIR__ . '/../stubs/ReflectionEnum.php';
+    require __DIR__ . '/../stubs/ReflectionEnumUnitCase.php';
+    require __DIR__ . '/../stubs/ReflectionEnumBackedCase.php';
+}
+
+require __DIR__ . '/../stubs/ReflectionIntersectionType.php';
+
 $GLOBALS['loader'] = require __DIR__ . '/../vendor/autoload.php';
