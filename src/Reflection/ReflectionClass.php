6b967b619a573277622a56ebb458423817cf4528
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-06 21:50:45 +0100

    Do not limit Stringable to internal classes

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -1245,10 +1245,6 @@ class ReflectionClass implements Reflection
      */
     private function addStringableInterface(array $interfaces): array
     {
-        if (BetterReflection::$phpVersion < 80000) {
-            return $interfaces;
-        }
-
         /** @psalm-var class-string $stringableClassName */
         $stringableClassName = Stringable::class;
 
@@ -1260,10 +1256,7 @@ class ReflectionClass implements Reflection
             if (strtolower($immediateMethodName) === '__tostring') {
                 try {
                     $stringableInterfaceReflection = $this->reflector->reflectClass($stringableClassName);
-
-                    if ($stringableInterfaceReflection->isInternal()) {
-                        $interfaces[$stringableClassName] = $stringableInterfaceReflection;
-                    }
+                    $interfaces[$stringableClassName] = $stringableInterfaceReflection;
                 } catch (IdentifierNotFound) {
                     // Stringable interface does not exist on target PHP version
                 }
