baa6266f9fc9ca97dec8622bcf7359fe377671c3
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-13 16:17:42 +0100

    Deprecate methods that do new BetterReflection()

--- src/Reflection/ReflectionFunction.php
+++ src/Reflection/ReflectionFunction.php
@@ -51,7 +51,11 @@ class ReflectionFunction implements Reflection
         $this->isGenerator = $this->nodeIsOrContainsYield($node);
     }
 
-    /** @throws IdentifierNotFound */
+    /**
+     * @deprecated Use Reflector instead.
+     *
+     * @throws IdentifierNotFound
+     */
     public static function createFromName(string $functionName): self
     {
         return (new BetterReflection())->reflector()->reflectFunction($functionName);
