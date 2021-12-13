050676d5c47ef05c6a5f2d2931c433c12a5b7a09
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-13 22:06:53 +0100

    Fix

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -1250,6 +1250,10 @@ class ReflectionClass implements Reflection
      */
     private function addStringableInterface(array $interfaces): array
     {
+        if (BetterReflection::$phpVersion < 80000) {
+            return $interfaces;
+        }
+
         /** @psalm-var class-string $stringableClassName */
         $stringableClassName = Stringable::class;
 
