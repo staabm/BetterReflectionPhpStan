77832f2f90b8543aacc5db470e5f4b55327bd854
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-19 15:32:50 +0100

    Attribute class is always defined

--- src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
+++ src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
@@ -394,7 +394,7 @@ final class PhpStormStubsSourceStubber implements SourceStubber
             [$classNode] = $classNodeData;
 
             if ($isCoreExtension) {
-                if ($className !== 'ReturnTypeWillChange' && ! $this->isSupportedInPhpVersion($classNode)) {
+                if ($className !== 'Attribute' && $className !== 'ReturnTypeWillChange' && ! $this->isSupportedInPhpVersion($classNode)) {
                     continue;
                 }
 
