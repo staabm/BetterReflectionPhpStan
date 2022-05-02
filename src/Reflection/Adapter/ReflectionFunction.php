7909edee2f93864b3b245442802d818a4597714f
Ondrej Mirtes <ondrej@mirtes.cz>
2022-05-02 10:26:02 +0200

    Adapter - use covariant return types

--- src/Reflection/Adapter/ReflectionFunction.php
+++ src/Reflection/Adapter/ReflectionFunction.php
@@ -169,6 +169,7 @@ final class ReflectionFunction extends CoreReflectionFunction
         return $this->betterReflectionFunction->hasReturnType();
     }
 
+    /** @return ReflectionUnionType|ReflectionNamedType|ReflectionIntersectionType|null */
     public function getReturnType(): CoreReflectionType|null
     {
         return ReflectionType::fromTypeOrNull($this->betterReflectionFunction->getReturnType());
@@ -256,6 +257,7 @@ final class ReflectionFunction extends CoreReflectionFunction
         return $this->betterReflectionFunction->hasTentativeReturnType();
     }
 
+    /** @return ReflectionUnionType|ReflectionNamedType|ReflectionIntersectionType|null */
     public function getTentativeReturnType(): CoreReflectionType|null
     {
         return ReflectionType::fromTypeOrNull($this->betterReflectionFunction->getTentativeReturnType());
