50c3aac579c149e6b9bf58ecc668c6daf719fcf3
Ondrej Mirtes <ondrej@mirtes.cz>
2022-05-13 08:32:15 +0200

    ReflectionClass - prioritize first method of the same name
    DatePeriod stub in phpstorm-stubs has multiple constructors

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -549,6 +549,10 @@ class ReflectionClass implements Reflection
                 $this,
             );
 
+            if (array_key_exists($method->getName(), $methods)) {
+                continue;
+            }
+
             $methods[$method->getName()] = $method;
         }
 
