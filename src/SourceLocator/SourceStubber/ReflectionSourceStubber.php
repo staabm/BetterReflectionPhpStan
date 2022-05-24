97338bda89e4f14bca7a917f56a80987e97f9eb1
Ondrej Mirtes <ondrej@mirtes.cz>
2022-05-24 10:48:56 +0200

    ReflectionSourceStubber - do not make optional parameter without default value available nullable

--- src/SourceLocator/SourceStubber/ReflectionSourceStubber.php
+++ src/SourceLocator/SourceStubber/ReflectionSourceStubber.php
@@ -607,7 +607,11 @@ final class ReflectionSourceStubber implements SourceStubber
         }
 
         if (! $parameterReflection->isDefaultValueAvailable()) {
-            $parameterNode->setDefault(null);
+            if ($parameterReflection->allowsNull()) {
+                $parameterNode->setDefault(null);
+            } else {
+                $parameterNode->setDefault(new Node\Expr\ConstFetch(new FullyQualified('UNKNOWN')));
+            }
 
             return;
         }
