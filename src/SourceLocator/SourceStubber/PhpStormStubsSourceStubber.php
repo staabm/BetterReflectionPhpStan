bfb54736f2007208b25ecfdf2badf77a8b32e4fc
Ondrej Mirtes <ondrej@mirtes.cz>
2022-07-01 13:32:23 +0200

    Allow AllowDynamicProperties on any PHP version

--- src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
+++ src/SourceLocator/SourceStubber/PhpStormStubsSourceStubber.php
@@ -400,7 +400,12 @@ final class PhpStormStubsSourceStubber implements SourceStubber
             [$classNode] = $classNodeData;
 
             if ($isCoreExtension) {
-                if ($className !== 'Attribute' && $className !== 'ReturnTypeWillChange' && ! $this->isSupportedInPhpVersion($classNode)) {
+                if (
+                    $className !== 'Attribute'
+                    && $className !== 'ReturnTypeWillChange'
+                    && $className !== 'AllowDynamicProperties'
+                    && ! $this->isSupportedInPhpVersion($classNode)
+                ) {
                     continue;
                 }
 
