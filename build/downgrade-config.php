2bfd697318c86d97a26553d754e48e1bc442c20b
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-26 14:48:12 +0200

    Update Rector

--- build/downgrade-config.php
+++ build/downgrade-config.php
@@ -1,15 +1,12 @@
 <?php declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::SKIP, [
+return static function (RectorConfig $config): void {
+    $config->skip([
         '*/test/unit/Fixture/*',
         'src/Reflection/Adapter/ReflectionEnum*'
     ]);
-    $containerConfigurator->import(DowngradeLevelSetList::DOWN_TO_PHP_73);
+    $config->sets([DowngradeLevelSetList::DOWN_TO_PHP_73]);
 };
