0937cc1a8f73d850755f06af48fa180a415a7371
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-26 14:35:55 +0200

    Test fixes

--- test/unit/Reflection/ReflectionClassTest.php
+++ test/unit/Reflection/ReflectionClassTest.php
@@ -2831,7 +2831,7 @@ PHP;
             $new->class,
             new LocatedSource($source, null),
         );
-        $array     = $anonymous->getConstant('CACHE_MAP');
+        $array     = $anonymous->getConstant('CACHE_MAP')->getValue();
         self::assertIsArray($array);
     }
 }
