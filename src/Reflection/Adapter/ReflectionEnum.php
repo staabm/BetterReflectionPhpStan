366c5177561c404d1ff6c30f2ed8e1cdf0bce234
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-26 14:57:14 +0200

    Easier downgrading

--- src/Reflection/Adapter/ReflectionEnum.php
+++ src/Reflection/Adapter/ReflectionEnum.php
@@ -110,7 +110,7 @@ final class ReflectionEnum extends CoreReflectionEnum
         return $this->betterReflectionEnum->getDocComment() ?? false;
     }
 
-    public function getConstructor(): CoreReflectionMethod|null
+    public function getConstructor(): ?CoreReflectionMethod
     {
         $constructor = $this->betterReflectionEnum->getConstructor();
 
@@ -512,8 +512,7 @@ final class ReflectionEnum extends CoreReflectionEnum
         return $this->betterReflectionEnum->implementsInterface($realInterfaceName);
     }
 
-    /** @return never */
-    public function getExtension(): CoreReflectionExtension|null
+    public function getExtension(): ?CoreReflectionExtension
     {
         throw new Exception\NotImplemented('Not implemented');
     }
