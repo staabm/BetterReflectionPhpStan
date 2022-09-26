b300998ec1b7b6367e8a5ccc0d6ef6cf8e480f99
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-26 15:14:17 +0200

    ReflectionClass - cache all the things!

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -139,9 +139,22 @@ class ReflectionClass implements Reflection
      */
     private array|null $cachedProperties = null;
 
+    /** @var array<class-string, ReflectionClass>|null */
+    private array|null $cachedInterfaces = null;
+
+    /** @var list<class-string>|null */
+    private array|null $cachedInterfaceNames = null;
+
+    /** @var list<ReflectionClass>|null */
+    private array|null $cachedTraits = null;
+
+    private ReflectionMethod|null $cachedConstructor = null;
+
+    private string|null $cachedName = null;
+
     /**
-     * @var array<lowercase-string, ReflectionMethod>|null
      * @psalm-allow-private-mutation
+     * @var array<lowercase-string, ReflectionMethod>|null
      */
     private array|null $cachedMethods = null;
 
@@ -321,14 +334,18 @@ class ReflectionClass implements Reflection
      */
     public function getName(): string
     {
+        if ($this->cachedName !== null) {
+            return $this->cachedName;
+        }
+
         if (! $this->inNamespace()) {
             /** @psalm-var class-string|trait-string */
-            return $this->getShortName();
+            return $this->cachedName = $this->getShortName();
         }
 
         assert($this->name !== null);
 
-        return $this->name;
+        return $this->cachedName = $this->name;
     }
 
     /** @return class-string|null */
@@ -839,9 +856,13 @@ class ReflectionClass implements Reflection
      */
     public function getConstructor(): ReflectionMethod|null
     {
+        if ($this->cachedConstructor !== null) {
+            return $this->cachedConstructor;
+        }
+
         $constructors = array_values(array_filter($this->getMethods(), static fn (ReflectionMethod $method): bool => $method->isConstructor()));
 
-        return $constructors[0] ?? null;
+        return $this->cachedConstructor = $constructors[0] ?? null;
     }
 
     /**
@@ -1302,7 +1323,11 @@ class ReflectionClass implements Reflection
      */
     public function getTraits(): array
     {
-        return array_map(
+        if ($this->cachedTraits !== null) {
+            return $this->cachedTraits;
+        }
+
+        return $this->cachedTraits = array_map(
             fn (string $traitClassName): ReflectionClass => $this->reflector->reflectClass($traitClassName),
             $this->traitClassNames,
         );
@@ -1605,6 +1630,10 @@ class ReflectionClass implements Reflection
      */
     public function getInterfaces(): array
     {
+        if ($this->cachedInterfaces !== null) {
+            return $this->cachedInterfaces;
+        }
+
         $interfaces = array_merge(
             [$this->getCurrentClassImplementedInterfacesIndexedByName()],
             array_map(
@@ -1613,7 +1642,7 @@ class ReflectionClass implements Reflection
             ),
         );
 
-        return array_merge(...array_reverse($interfaces));
+        return $this->cachedInterfaces = array_merge(...array_reverse($interfaces));
     }
 
     /**
@@ -1648,7 +1677,11 @@ class ReflectionClass implements Reflection
      */
     public function getInterfaceNames(): array
     {
-        return array_values(array_map(
+        if ($this->cachedInterfaceNames !== null) {
+            return $this->cachedInterfaceNames;
+        }
+
+        return $this->cachedInterfaceNames = array_values(array_map(
             static fn (self $interface): string => $interface->getName(),
             $this->getInterfaces(),
         ));
