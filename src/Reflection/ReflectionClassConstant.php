63ec9b599256a04093a2b87f7f99cc712417fdf5
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-27 14:02:55 +0200

    Fix non-existent constants on lower PHP versions

--- src/Reflection/ReflectionClassConstant.php
+++ src/Reflection/ReflectionClassConstant.php
@@ -188,7 +188,7 @@ class ReflectionClassConstant
      */
     public function isPublic(): bool
     {
-        return ($this->modifiers & CoreReflectionClassConstant::IS_PUBLIC) === CoreReflectionClassConstant::IS_PUBLIC;
+        return ($this->modifiers & ReflectionClassConstantAdapter::IS_PUBLIC) === ReflectionClassConstantAdapter::IS_PUBLIC;
     }
 
     /**
@@ -197,7 +197,7 @@ class ReflectionClassConstant
     public function isPrivate(): bool
     {
         // Private constant cannot be final
-        return $this->modifiers === CoreReflectionClassConstant::IS_PRIVATE;
+        return $this->modifiers === ReflectionClassConstantAdapter::IS_PRIVATE;
     }
 
     /**
@@ -205,7 +205,7 @@ class ReflectionClassConstant
      */
     public function isProtected(): bool
     {
-        return ($this->modifiers & CoreReflectionClassConstant::IS_PROTECTED) === CoreReflectionClassConstant::IS_PROTECTED;
+        return ($this->modifiers & ReflectionClassConstantAdapter::IS_PROTECTED) === ReflectionClassConstantAdapter::IS_PROTECTED;
     }
 
     public function isFinal(): bool
