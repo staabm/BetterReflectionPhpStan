ssvet/BetterReflection

Be more resilient against non-existent traits and interfaces

src/Reflection/ReflectionClass.php
@@ -1197,10 +1197,17 @@ class ReflectionClass implements Reflection
         if ($this->cachedTraits !== null) {
             return $this->cachedTraits;
         }
-        return $this->cachedTraits = array_values(array_map(
-            fn (string $importedTrait): ReflectionClass => $this->reflector->reflectClass($importedTrait),
-            $this->traitNames,
-        ));
+
+        $traits = [];
+        foreach ($this->traitNames as $traitName) {
+            try {
+                $traits[] = $this->reflector->reflectClass($traitName);
+            } catch (IdentifierNotFound) {
+                // pass
+            }
+        }
+
+        return $this->cachedTraits = $traits;
     }
 
     /**
@@ -1463,11 +1470,11 @@ class ReflectionClass implements Reflection
         foreach ($this->implementsNames as $name) {
             try {
                 $interface = $this->reflector->reflectClass($name);
+                foreach ($interface->getInterfacesHierarchy() as $n => $i) {
+                    $interfaces[$n] = $i;
+                }
             } catch (IdentifierNotFound $e) {
                 continue;
-            }
-            foreach ($interface->getInterfacesHierarchy() as $n => $i) {
-                $interfaces[$n] = $i;
             }
         }
 
@@ -1510,16 +1517,19 @@ class ReflectionClass implements Reflection
             throw NotAnInterfaceReflection::fromReflectionClass($this);
         }
 
-        /** @var array<class-string, self> */
-        return array_merge(
-            [$this->getName() => $this],
-            ...array_map(
-                fn (string $interfaceName): array => $this
-                    ->reflector->reflectClass($interfaceName)
-                    ->getInterfacesHierarchy(),
-                $this->interfaceExtendsNames,
-            ),
-        );
+        $interfaces = [$this->getName() => $this];
+        foreach ($this->interfaceExtendsNames as $interfaceName) {
+            try {
+                $interface = $this->reflector->reflectClass($interfaceName);
+                foreach ($interface->getInterfacesHierarchy() as $hName => $hInterface) {
+                    $interfaces[$hName] = $hInterface;
+                }
+            } catch (IdentifierNotFound) {
+                continue;
+            }
+        }
+
+        return $interfaces;
     }
 
     /**

staabm/BetterReflectionPhpStan

src/Reflection/ReflectionClass.php
@@ -42,7 +42,6 @@ use Stringable;
 use Traversable;
 use UnitEnum;
 
-use function array_combine;
 use function array_filter;
 use function array_key_exists;
 use function array_keys;
@@ -1263,10 +1262,16 @@ class ReflectionClass implements Reflection
             return $this->cachedTraits;
         }
 
-        return $this->cachedTraits = array_map(
-            fn (string $traitClassName): ReflectionClass => $this->reflector->reflectClass($traitClassName),
-            $this->traitClassNames,
-        );
+        $traits = [];
+        foreach ($this->traitClassNames as $traitClassName) {
+            try {
+                $traits[] = $this->reflector->reflectClass($traitClassName);
+            } catch (IdentifierNotFound) {
+                // pass
+            }
+        }
+
+        return $this->cachedTraits = $traits;
     }
 
     /**
@@ -1521,13 +1526,14 @@ class ReflectionClass implements Reflection
             return [];
         }
 
-        $interfaces = array_combine(
-            $this->implementsClassNames,
-            array_map(
-                fn (string $interfaceClassName): ReflectionClass => $this->reflector->reflectClass($interfaceClassName),
-                $this->implementsClassNames,
-            ),
-        );
+        $interfaces = [];
+        foreach ($this->implementsClassNames as $interfaceClassName) {
+            try {
+                $interfaces[$interfaceClassName] = $this->reflector->reflectClass($interfaceClassName);
+            } catch (IdentifierNotFound) {
+                continue;
+            }
+        }
 
         if ($this->isEnum) {
             $interfaces = $this->addEnumInterfaces($interfaces);
@@ -1670,15 +1676,17 @@ class ReflectionClass implements Reflection
             return array_slice($this->getInterfacesHierarchy(AlreadyVisitedClasses::createEmpty()), 1);
         }
 
-        $interfaces = array_merge(
-            [],
-            ...array_map(
-                fn (string $interfaceClassName): array => $this->reflector
-                    ->reflectClass($interfaceClassName)
-                    ->getInterfacesHierarchy(AlreadyVisitedClasses::createEmpty()),
-                $this->implementsClassNames,
-            ),
-        );
+        $interfaces = [];
+        foreach ($this->implementsClassNames as $name) {
+            try {
+                $interface = $this->reflector->reflectClass($name);
+                foreach ($interface->getInterfacesHierarchy(AlreadyVisitedClasses::createEmpty()) as $n => $i) {
+                    $interfaces[$n] = $i;
+                }
+            } catch (IdentifierNotFound) {
+                continue;
+            }
+        }
 
         if ($this->isEnum) {
             $interfaces = $this->addEnumInterfaces($interfaces);