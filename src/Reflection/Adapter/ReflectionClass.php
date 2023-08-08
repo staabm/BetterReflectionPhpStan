e348429fefde4297066d535fe248db9432a5d9ea
Markus Staab <markus.staab@redaxo.de>
2023-08-08 15:54:04 +0200

    Fix typo

--- src/Reflection/Adapter/ReflectionClass.php
+++ src/Reflection/Adapter/ReflectionClass.php
@@ -384,7 +384,7 @@ final class ReflectionClass extends CoreReflectionClass
     /** @return list<class-string> */
     public function getInterfaceClassNames(): array
     {
-        return $this->betterReflectionClass->getInterfaceNames();
+        return $this->betterReflectionClass->getInterfaceClassNames();
     }
 
     /**
