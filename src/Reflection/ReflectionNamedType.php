bf53c2ce0f027d5ea2937d6207e9731163d6ebcf
Ondrej Mirtes <ondrej@mirtes.cz>
2023-12-30 20:21:56 +0100

    ReflectionNamedType::isIdentifier()

--- src/Reflection/ReflectionNamedType.php
+++ src/Reflection/ReflectionNamedType.php
@@ -44,7 +44,7 @@ class ReflectionNamedType extends ReflectionType
     public function __construct(
         private Reflector $reflector,
         private ReflectionParameter|ReflectionMethod|ReflectionFunction|ReflectionEnum|ReflectionProperty|ReflectionClassConstant $owner,
-        Identifier|Name $type,
+        private Identifier|Name $type,
     ) {
         $this->name = $type->toString();
     }
@@ -123,6 +123,11 @@ class ReflectionNamedType extends ReflectionType
             'null' => true,
             default => false,
         };
+    }
+
+    public function isIdentifier(): bool
+    {
+        return $this->type instanceof Identifier;
     }
 
     /** @return non-empty-string */
