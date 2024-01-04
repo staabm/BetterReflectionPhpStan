enumag/BetterReflection

ReflectionClass - prioritize first method of the same name
DatePeriod stub in phpstorm-stubs has multiple constructors

src/Reflection/ReflectionClass.php
@@ -532,6 +532,10 @@ class ReflectionClass implements Reflection
                 continue;
             }
 
+            if (array_key_exists($method->getName(), $methodsByName)) {
+                continue;
+            }
+
             $methodsByName[$method->getName()] = $method;
         }
 

staabm/BetterReflectionPhpStan

src/Reflection/ReflectionClass.php
@@ -552,6 +552,10 @@ class ReflectionClass implements Reflection
                 $this,
             );
 
+            if (array_key_exists($method->getName(), $methods)) {
+                continue;
+            }
+
             $methods[$method->getName()] = $method;
         }
 