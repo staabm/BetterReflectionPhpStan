1dd80b83727313a7b6a3873ba9b39f77643c2906
Ondrej Mirtes <ondrej@mirtes.cz>
2024-03-26 19:02:40 +0100

    Fix

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -1185,7 +1185,11 @@ class ReflectionClass implements Reflection
             throw CircularReference::fromClassName($parentClassName);
         }
 
-        return $this->reflector->reflectClass($parentClassName);
+        try {
+            return $this->reflector->reflectClass($parentClassName);
+        } catch (IdentifierNotFound) {
+            return null;
+        }
     }
 
     /**
