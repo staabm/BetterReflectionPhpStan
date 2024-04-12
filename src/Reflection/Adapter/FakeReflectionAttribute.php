d1deaa92bc48972b687cfa467984def68a8055e7
Jan Nedbal <janedbal@gmail.com>
2024-04-12 12:26:57 +0200

    FakeReflectionAttribute: undeprecate newInstance method

--- src/Reflection/Adapter/FakeReflectionAttribute.php
+++ src/Reflection/Adapter/FakeReflectionAttribute.php
@@ -43,7 +43,6 @@ final class FakeReflectionAttribute
         return $this->betterReflectionAttribute->getArgumentsExpressions();
     }
 
-    /** @deprecated */
     public function newInstance(): object
     {
         $class = $this->getName();
