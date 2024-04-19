5f52ce1e2491c94cb1e162e5b76b04851648e01d
Jan Nedbal <janedbal@gmail.com>
2024-04-19 15:32:45 +0200

    undeprecate also in ReflectionAttribute

--- src/Reflection/Adapter/ReflectionAttribute.php
+++ src/Reflection/Adapter/ReflectionAttribute.php
@@ -60,7 +60,6 @@ final class ReflectionAttribute extends CoreReflectionAttribute
         return $this->betterReflectionAttribute->getArgumentsExpressions();
     }
 
-    /** @deprecated */
     public function newInstance(): object
     {
         $class = $this->getName();
