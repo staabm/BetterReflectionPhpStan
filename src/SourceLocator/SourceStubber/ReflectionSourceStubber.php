bcaa5cc64869398bffc6420f23c210bcb2d82667
Ondrej Mirtes <ondrej@mirtes.cz>
2024-08-26 22:21:22 +0200

    Fix compatibility with 8.4

--- src/SourceLocator/SourceStubber/ReflectionSourceStubber.php
+++ src/SourceLocator/SourceStubber/ReflectionSourceStubber.php
@@ -141,7 +141,11 @@ final class ReflectionSourceStubber implements SourceStubber
 
     public function generateFunctionStubFromReflection(CoreReflectionFunction $functionReflection): ?StubData
     {
-        $functionNode = $this->builderFactory->function($functionReflection->getShortName());
+        $shortName = $functionReflection->getShortName();
+        if ($functionReflection->isClosure()) {
+            $shortName = '{closure}';
+        }
+        $functionNode = $this->builderFactory->function($shortName);
 
         $this->addDocComment($functionNode, $functionReflection);
         $this->addParameters($functionNode, $functionReflection);
@@ -156,7 +160,7 @@ final class ReflectionSourceStubber implements SourceStubber
         $extensionName = $functionReflection->getExtension()?->getName();
         assert((is_string($extensionName) && $extensionName !== '') || $extensionName === null);
 
-        if (! $functionReflection->inNamespace()) {
+        if (! $functionReflection->inNamespace() || $functionReflection->isClosure()) {
             return $this->createStubData($this->generateStub($functionNode->getNode()), $extensionName, $functionReflection->getFileName() !== false ? $functionReflection->getFileName() : null);
         }
 
