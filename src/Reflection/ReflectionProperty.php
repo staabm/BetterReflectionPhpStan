25c94c96046c793927e71c875bc4c0e1ccd1fa19
Ondrej Mirtes <ondrej@mirtes.cz>
2025-01-04 13:23:09 +0100

    Fix

--- src/Reflection/ReflectionProperty.php
+++ src/Reflection/ReflectionProperty.php
@@ -675,7 +675,7 @@ class ReflectionProperty
             && ! $node->isPublicSet()
             && $node->isPublic()
             && (
-                ($modifiers & ReflectionPropertyAdapter::IS_READONLY)
+                ($modifiers & ReflectionPropertyAdapter::IS_READONLY_COMPATIBILITY)
                 || $this->getDeclaringClass()->isReadOnly()
             )
         ) {
