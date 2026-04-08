195c4129d2098ebf9bd3fbe6e00bd51b4820baee
Ondrej Mirtes <ondrej@mirtes.cz>
2026-04-08 10:15:56 +0200

    Revert "ExprCacheHelper: Reduce PHPParser instantiation"
    This reverts commit 9deb73d541acdf44537f1e9b33d46e9d95764b4c.

--- src/Reflection/ExprCacheHelper.php
+++ src/Reflection/ExprCacheHelper.php
@@ -9,14 +9,13 @@ use Roave\BetterReflection\BetterReflection;
 
 final class ExprCacheHelper
 {
-    private static ?BetterReflection $betterReflection = null;
 
     /**
-     * @return array<string, mixed>
+     * @return array<string, mixed
      */
     public static function export(Expr $expr): array
     {
-        $br = self::$betterReflection ??= new BetterReflection();
+        $br = new BetterReflection();
 
         $attributes = [];
         foreach (['startLine', 'endLine', 'startTokenPos', 'startFilePos', 'endTokenPos', 'endFilePos'] as $key) {
@@ -34,9 +33,8 @@ final class ExprCacheHelper
         $code = $data['code'];
         $attributes = $data['attributes'];
 
-        $br = self::$betterReflection ??= new BetterReflection();
-        // cached parser implementations might re-use nodes. clone so we don't mix up attributes.
-        $expr = clone $br->phpParser()->parse('<?php ' . $code . ';')[0]->expr;
+        $br = new BetterReflection();
+        $expr = $br->originalPhpParser()->parse('<?php ' . $code . ';')[0]->expr;
         foreach ($attributes as $key => $value) {
             $expr->setAttribute($key, $value);
         }
